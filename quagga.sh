5bf4f4a032415f0fed9555efd62cdf2cd8b19633
Hyunsun Moon <hyunsun.mooon@gmail.com>
2016-07-12 22:27:29 -0400

    Update

--- quagga.sh
+++ quagga.sh
@@ -2,7 +2,7 @@
 
 # Prints usage help
 function usage {
-    echo "usage: quagga.sh --name=gateway-01 --ip=172.18.0.254/24 --mac=fe:00:00:00:00:01 [--bridge=br-router]" >&2
+    echo "usage: quagga.sh --name=gateway-01 --ip=172.18.0.254/24 --mac=fe:00:00:00:00:01 [--bridge=br-router] [--external-router]" >&2
     echo "       -h --help"
     echo "       -n --name    name of the quagga container"
     echo "       -i --ip      IP address for peering"
@@ -31,8 +31,8 @@ while [ "$1" != "" ]; do
         -b | --bridge)
             BRIDGE_NAME=$VALUE
             ;;
-        --no-default)
-            DEFAULT=false
+        --external-router)
+            EXTERNAL_ROUTER=true
             ;;
         *)
             echo "ERROR: unknown parameter \"$PARAM\""
@@ -62,13 +62,22 @@ if [ -z $BRIDGE_NAME ]; then
   BRIDGE_NAME="br-router"
 fi
 
+if [ -z $EXTERNAL_ROUTER ]; then
+  PORT_NAME="quagga"
+  VOLUME_DIR=~/sona-setup/volumes/gateway
+  POST_COMMAND="route del default gw 172.17.0.1"
+else
+  PORT_NAME="quagga-router"
+  VOLUME_DIR=~/sona-setup/volumes/router
+  POST_COMMAND="iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE"
+fi
+
 # clean up existing container with same name and IP
 sudo docker pull hyunsun/quagga-fpm
 sudo docker stop $CONTAINER_NAME
 sudo docker rm $CONTAINER_NAME
-sudo ovs-vsctl del-port quagga
+sudo ovs-vsctl del-port $PORT_NAME
 
-sudo docker run --privileged --cap-add=NET_ADMIN --cap-add=NET_RAW --name $CONTAINER_NAME --hostname $CONTAINER_NAME -d -v ~/sona-setup/quagga:/usr/etc hyunsun/quagga-fpm
-sudo ~/sona-setup/pipework $BRIDGE_NAME -i eth1 -l quagga $CONTAINER_NAME $ETH1_IP_CIDR $ETH1_MAC
-sudo docker exec -d $CONTAINER_NAME iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-sudo docker exec -d $CONTAINER_NAME route del default gw 172.17.0.1
+sudo docker run --privileged --cap-add=NET_ADMIN --cap-add=NET_RAW --name $CONTAINER_NAME --hostname $CONTAINER_NAME -d -v $VOLUME_DIR:/usr/etc hyunsun/quagga-fpm
+sudo ~/sona-setup/pipework $BRIDGE_NAME -i eth1 -l $PORT_NAME $CONTAINER_NAME $ETH1_IP_CIDR $ETH1_MAC
+sudo docker exec -d $CONTAINER_NAME $POST_COMMAND
